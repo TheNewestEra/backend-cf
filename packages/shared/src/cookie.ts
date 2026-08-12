6be60811e3edd60b2259d45ea016b7076a81d8e4
Ryan-Blignaut-BBD <ryan.blignaut@bbd.co.za>
2026-08-12 22:28:17 +0200

    cookie again test

--- packages/shared/src/cookie.ts
+++ packages/shared/src/cookie.ts
@@ -7,8 +7,8 @@ export function sessionCookieOpts(allowInsecureLocalDev: boolean) {
         path: "/",
         domain: allowInsecureLocalDev ? undefined : COOKIE_DOMAIN,
         httpOnly: true,
-        secure: !allowInsecureLocalDev,
-        sameSite: "Lax",
+        secure: true,
+        sameSite: allowInsecureLocalDev? "none": "Lax",
         maxAge: 60 * 60 * 24 * 30,
     } as const;
 }
